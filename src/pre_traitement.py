35342828e3b0334a2892ccf789425f381d2aa206
thierry pouplier <thierrypouplier@gmail.com>
2017-05-26 16:18:43 -0400

    Removed wrong data, added folders and bare files

---
+++ src/pre_traitement.py
@@ -0,0 +1,24 @@
+import os
+
+def main():
+    {
+        os.chdir("../data/test/")
+    }
+
+def filter_static_energy():
+    {
+
+    }
+
+def filter_dynamic_energy():
+    {
+
+    }
+
+def filter_combined_energy():
+    {
+
+    }
+
+if __name__ == '__main__':
+    main()
