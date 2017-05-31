857b0e5e0da9456e6eef2d015bf6663e065e374a
thierry pouplier <thierrypouplier@gmail.com>
2017-05-30 22:17:12 -0400

    WIP

--- src/pre_traitement.py
+++ src/pre_traitement.py
@@ -1,24 +1,49 @@
 import os
+import yaml
+
+class PreTraitement: # Définition de notre classe PreTraitrement
+
+    def __init__(self): # Constructeur
+        script_dir = os.path.dirname(__file__)
+        rel_path = "config/pre_traitement.yaml"
+        abs_file_path = os.path.join(script_dir, rel_path)
+
+        stream = open("test", "r")
+        docs = yaml.load_all(stream)
+        for doc in docs:
+            for k, v in doc.items():
+                print(k, "->", v)
+                print("\n")
+
+        file = open(abs_file_path, 'r')
+        line = [map(int, line.split(' ')) for line in file]
+        print(line)
+
+        self.test_data_path =
+
+    def fetch_data(self):
+        script_dir = os.path.dirname(__file__)
+        rel_path = ".." +
+        abs_file_path = os.path.join(script_dir, rel_path)
+
+        f = open('input.txt', 'r')
+        l = [map(int, line.split(' ')) for line in f]
+        print(l)
+
+
+    def filter_static_energy(self):
+
+
+    def filter_dynamic_energy(self):
+
+
+    def filter_combined_energy(self):
+
 
 def main():
-    {
-        os.chdir("../data/test/")
-    }
+    pre_traitement = PreTraitement()
 
-def filter_static_energy():
-    {
-
-    }
-
-def filter_dynamic_energy():
-    {
-
-    }
-
-def filter_combined_energy():
-    {
-
-    }
+    pre_traitement.fetch_data()
 
 if __name__ == '__main__':
     main()
