ada82b82ae35ad354ba5f5217f1377d71b60fac3
Mhamed Tazi <taziyul@hotmail.com>
2017-06-06 23:31:24 -0400

    Open_file_and_Signal_processing
    this code is intended to open one file read the full content and convert
    the string variabes into float variables.

---
+++ src/mpl.py
@@ -0,0 +1,31 @@
+import math
+import numpy
+
+with open(r'C:\Users\Tazi\Documents\ETS\ELE778\Labo2\ele778_lab2\data\raw\train\man\ae\1a.txt', 'r') as content_file:
+    content = content_file.read()
+    #content = content_file.readline().split(' ')
+##content = content_file.read().split()
+    #map(float,content)
+print (content)
+#print(type(content))
+#print (content[0])
+#print (content[1])
+#self: [(content)]
+
+print (float(content[1]))
+
+#type(content[0])
+#print(type(content[2]))
+#print(float.content[0]," ",float.content[1])
+
+#numpy.operator.__add__(float.content[0], float.content[1])
+print (len (content))
+#float (content[1])
+
+print(type(content[1]))
+#z=x+y
+#print(type(x))
+#print(type(y))
+#print(z)
+content_file.close()
+
