d5c00c6d3bb7513b2b26a6f44099595ce0f1306f
thierry pouplier <thierrypouplier@gmail.com>
2017-07-18 03:08:55 -0400

    Fixed data fetching

--- src/mlp.py
+++ src/mlp.py
@@ -7,41 +7,57 @@ import numpy as np
 import time
 
 
-class mlp:  # Definition de notre classe mlp
+class MLP:  # Definition de notre classe mlp
 
     def __init__(self, nb_layer, nb_hidden, nb_input, nb_output, learning_rate = 0.05, momentum = 0.5):
         # TODO Seed random for debug
         np.random.seed(1)
 
-        # Creating layers
-        W = [nb_layer]
-        B = [nb_layer]
-        nb_in = nb_input
-        nb_out = nb_hidden
-        for i in range(nb_layer, -1, -1):
-            W[i] = np.random.normal(scale=0.1, size=(nb_in, nb_out))
-            B[i] = np.random.normal(scale=0.1, size=(nb_out))
-            nb_in = nb_out
-            if i == 0:
-                nb_out = nb_output
-            else:
-                nb_out = nb_hidden
+        # Init variables
+        self.nb_layer_ = nb_layer
+        self.W_ = [self.nb_layer_]      # ex: nb_layer_ = 2 --> W_ = [ , ]
+        self.B_ = [self.nb_layer_]
+        self.nb_input_ = nb_input
+        self.nb_output_ = nb_output
+        self.nb_hidden_ = nb_hidden
 
+        # Creating layers
+        layer_in = self.nb_input_
+        layer_out = self.nb_hidden_
+        for i in range(nb_layer-1):
+            self.W[i] = np.random.normal(scale=0.1, size=(layer_in, layer_out))
+            self.B[i] = np.random.normal(scale=0.1, size=(layer_out))
+
+            # Preparing variable for next layer
+            layer_in = layer_out
+            if i == 0:
+                layer_out = self.nb_output_
+            else:
+                layer_out = self.nb_hidden_
 
     # Activation function 0 to 1
     @staticmethod
-    def sigmoide(self,x):
-        return 1/(1/np.exp(-x))
+    def sigmoide(self, x):
+        return 1/(1/np.exp(-x))\
+
+    @staticmethod
+    def sigmoide_prime(self, x):
+        return np.ex(-x)/((1+np.exp(-x))**2)
 
     # Activation function -1 to 1
+    @staticmethod
+    def tanh(self,x):
+        return np.tanh(x)
+
     @staticmethod
     def tanh_prime(self,x):
         return 1 - np.tanh(x)**2
 
-    def max(self):
-
-
-    def predict(x):
+# #TODO
+#     def max(self):
+#
+# #TODO
+#     def predict(x):
 
 
 
--- src/trainer.py
+++ src/trainer.py
@@ -3,47 +3,99 @@ import os
 import sys
 import yaml
 import functools
-import trainer.py
+import numpy as np
+import mlp
+import data_manager
+import pickle
+import random
 
-class trainer:  # Definition de notre classe PreTraitrement
 
-    def __init__(self):  # Constructeur
+class Trainer:
+    def __init__(self, *existing_mlp):  # Constructeur
         # Hyperparameters
-        learning_rate = 0.01
-        momentum = 0.9
+        self.learning_rate = 0.01
+        self.momentum = 0.9
+        self.batch_size = 50
+        self.n_epoch = 100
+        self.activation_function = "sigmoid"
 
-    # input data, transpose, layer1, layer2, biases
-    def train(self, mlp):
+        # Supervising matrix
+        D = np.zeros((10, 10), float)
+        np.fill_diagonal(D, 1)
 
-        def feedFoward(self):
-            for layer in mlp.W:
-                i = np.dot(x, V) + bv
-                A += mlp.tanh_prime(i)
+        self.data_manager = data_manager.DataManager()
+        self.data_tree = None
+        self.batchs = [self.n_epoch]
 
-        def computeError(self):
+        # if existing_mlp:
+        #     self.mlp = existing_mlp
+        # else:
+        #     self.mlp = mlp.MLP()
 
-        def computeDelta(self):
-            for
+    # Create all the epoch baths, need to know: combined, static or dynamic data ?
+    def create_batch(self, witch_filter):
+        # Load data
+        if witch_filter == "combined":
+            self.data_manager.fetch_data(self.data_manager.trees["combined"],
+                                         os.path.join(self.data_manager.paths["abs_filtered_data_path"], "combined"))
+            self.data_tree = self.data_manager.trees["combined"]
+        elif witch_filter == "static":
+            self.data_manager.fetch_data(self.data_manager.trees["static"],
+                                         os.path.join(self.data_manager.paths["abs_filtered_data_path"], "static"))
+            self.data_tree = self.data_manager.trees["static"]
+        elif witch_filter == "dynamic":
+            self.data_manager.fetch_data(self.data_manager.trees["dynamic"],
+                                         os.path.join(self.data_manager.paths["abs_filtered_data_path"], "dynamic"))
+            self.data_tree = self.data_manager.trees["dynamic"]
+        else:
+            return False
+
+        if self.activation_function == "sigmoid":
+            self.data_manager.normalize_tree(self.data_tree, 0, 1)
+        elif self.activation_function == "tanh":
+            self.data_manager.normalize_tree(self.data_tree, -1, 1)
+
+        for batch in self.batchs:
+            batch = np.array()
+            for data in batch:
+                self.data_manager.find_random_and(self.data_tree["test"], self.data_manager.np_concatonate, batch)
+
+    # input data, transpose, layers, biases, mlp obj
+    def train(self, x, t, W, B, mlp):
+
+        for epoch in range(self.n_epoch):
+            # feedFoward
+            a = x
+            for layer_nb in range(mlp.nb_layer_):
+                i = np.dot(a, mlp.W_[layer_nb]) + mlp.B_[layer_nb]
+                a = mlp.tanh_prime(i)
 
 
-        def backprop(self):
-            for layer in reversed(layers):
-                W = W + dw
+                # # Compute Error and delta
+                # for layer_nb in range(mlp.nb_layer_, -1, -1):
+                #     if layer_nb == mlp.nb_layer_:
+                #         s = np.multiply(np.multiply(np.subtract(d, a), a), np.subtract(1, a))
+                #     else:
+                #         s = np.multiply(a, np.subtract(1, a))
+                #
+                #     if layer_nb == 1:
+                #         dw = np.
+                #     else:
+                #         dw =
+                #
+                # # backprop
+                # for layer in reversed(layers):
+                #     W = W + dw
 
-        for epoch in range(n_epoch):
-            self.feedFoward()
-            self.computeError()
-            self.computeDelta()
-            self.backprop()
 
-
-def main()
+def main():
     # Set logging config
     logging.basicConfig(stream=sys.stderr, level=logging.INFO)  # DEBUG to debug, INFO to turn off
     logger = logging.getLogger(__name__)
 
+    trainer = Trainer()
 
-
+    trainer.create_batch("static")
 
 
 if __name__ == '__main__':
